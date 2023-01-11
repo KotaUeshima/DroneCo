da02aa2969441010d20930c9fd161bbb70674396
Kota Ueshima <ueshima@usc.edu>
2023-01-11 15:07:26 -0800

    changed form color

--- homePage/ContactUs.js
+++ homePage/ContactUs.js
@@ -12,7 +12,7 @@ function ContactUs() {
         <form
           action="https://getform.io/f/dfe9a9e9-6b9c-433a-9867-96d178be9e94"
           method="POST"
-          className="flex flex-col w-2/3 bg-droneBlue px-20 pb-10"
+          className="flex flex-col w-2/3 border-[1px] border-droneBlue bg-gray-50 px-20 pb-10 drop-shadow-2xl"
         >
           <div className="my-8 h-16 w-full flex justify-center items-center">
             <div className="relative h-full w-full">
@@ -29,46 +29,46 @@ function ContactUs() {
               type="text"
               name="firstName"
               placeholder="First Name*"
-              className="p-2 bg-white border-2 w-1/2 text-black focus:outline-none"
+              className="font-thin p-2 bg-white border-2 w-1/2 text-black focus:outline-none"
             />
             <input
               type="text"
               name="lastName"
               placeholder="Last Name*"
-              className="p-2 bg-white border-2 w-1/2 text-black focus:outline-none"
+              className="font-thin p-2 bg-white border-2 w-1/2 text-black focus:outline-none"
             />
           </div>
           <input
             type="text"
             name="email"
             placeholder="Email*"
-            className="mt-4 p-2 bg-white border-2  text-black focus:outline-none"
+            className="font-thin mt-4 p-2 bg-white border-2  text-black focus:outline-none"
           />
           <input
             type="text"
             name="companyName"
             placeholder="Company Name*"
-            className="mt-4 p-2 bg-white border-2  text-black focus:outline-none"
+            className="font-thin mt-4 p-2 bg-white border-2  text-black focus:outline-none"
           />
           <input
             type="text"
             name="phoneNumber"
             placeholder="Phone Number*"
-            className="mt-4 p-2 bg-white border-2  text-black focus:outline-none"
+            className="font-thin mt-4 p-2 bg-white border-2  text-black focus:outline-none"
           />
           <textarea
             name="additionalInfo"
             placeholder="Additional Info (optional)"
             rows="4"
-            className="mt-4 p-2 bg-white border-2  text-black focus:outline-none"
+            className="font-thin mt-4 p-2 bg-white border-2  text-black focus:outline-none"
           ></textarea>
-          <button className="mt-6 mx-auto bg-droneOrange hover:bg-transparent px-1 py-2 w-1/2  hover:text-white text-white border-[1px] hover:border-[1px] border-droneOrange hover:border-white duration-500">
-            Submit
+          <button className="font-thin mt-6 mx-auto bg-droneOrange hover:bg-transparent px-1 py-2 w-1/2  hover:text-droneOrange text-white border-[1px] hover:border-[1px] border-droneOrange hover:border-droneOrange duration-500">
+            Contact Us
           </button>
         </form>
       </div>
       <div className="h-[100vh] w-[30vw] flex flex-col justify-center items-center">
-        <h2 className="mt-12 text-4xl font-medium text-center">
+        <h2 className="mt-12 text-4xl font-light text-center">
           Elevate your Business with DroneCo.
         </h2>
         <p className="text-center text-gray-400">

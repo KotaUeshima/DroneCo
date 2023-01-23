d453bbe5261732ecd03969e46e08bdf5b7fb7011
Kota Ueshima <ueshima@usc.edu>
2023-01-23 10:56:00 -0800

    m

--- homePage/Drone.js
+++ homePage/Drone.js
@@ -1,10 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import modelX from "../public/assets/modelX.png";
-import modelY from "../public/assets/modelY.png";
 import modelY2 from "../public/assets/modelY2.png";
+import Typewriter from "typewriter-effect";
+import { useInView } from "react-intersection-observer";
 
 function Drone() {
+  const [x, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.25,
+  });
+
+  const [y, inView2] = useInView({
+    triggerOnce: true,
+    threshold: 0.25,
+  });
+
   return (
     <div
       name="drone"
@@ -14,9 +25,24 @@ function Drone() {
         <h2 className="-mt-10 md:-mt-20 text-right mr-10 text-[20vw] text-droneBlue">
           Model X
         </h2>
-        <p className="-mt-10 md:-mt-20 absolute right-10 w-1/3 text-right paragraph">
+        <p
+          ref={x}
+          className="-mt-10 md:-mt-20 absolute right-10 w-1/3 text-right paragraph"
+        >
           Suitable for lightweight QSRs and Pharmacy deliveries.
         </p>
+        {inView && (
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString("Compact and Lightweight. (<2lbs)").start();
+            }}
+            options={{
+              wrapperClassName: "-mt-10 absolute right-10 paragraph",
+              cursorClassName: "hidden",
+              delay: 100,
+            }}
+          />
+        )}
         <div className="absolute top-0 left-0 z-10 h-[65vh] md:h-[75vh] w-[50vw]">
           <Image
             src={modelX}
@@ -31,9 +57,25 @@ function Drone() {
         <h2 className="-mt-10 md:-mt-20 text-left ml-4 text-[20vw] text-droneBlue">
           Model Y
         </h2>
-        <p className="-mt-10 md:-mt-20 absolute left-6 md:left-10 w-1/3 text-left paragraph">
+        <p
+          ref={y}
+          className="-mt-10 md:-mt-20 absolute left-6 md:left-10 w-1/3 text-left paragraph"
+        >
           Suitable for standard Quick Commerce and Retail deliveries.
         </p>
+        {inView2 && (
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter.typeString("Compact and Lightweight. (<5lbs)").start();
+            }}
+            options={{
+              wrapperClassName: "absolute left-6 md:left-10 paragraph",
+              cursorClassName: "hidden",
+              delay: 100,
+            }}
+          />
+        )}
+
         <div className="absolute top-6 right-5 z-10 h-[65vh] md:h-[75vh] w-[55vw]">
           <Image
             src={modelY2}
--- pages/careers/TeamPage.js
+++ pages/careers/TeamPage.js
@@ -1,37 +1,48 @@
 import React from "react";
 import Image from "next/image";
-import Kota from "../../public/assets/KotaProfile.jpg";
+import Kota from "../../public/assets/teamPics/17.png";
+import Atharva from "../../public/assets/teamPics/14.png";
+import Tushar from "../../public/assets/teamPics/16.png";
+import Shriya from "../../public/assets/teamPics/15.png";
+import Sowmiya from "../../public/assets/teamPics/18.png";
 
 function TeamPage() {
   const teamMembers = [
     {
       id: 1,
-      name: "Kota Ueshima",
-      position: "Software Engineer | Frontend Developer",
-      img: Kota,
+      name: "Tushar Gupta",
+      position: "Founder",
+      img: Tushar,
     },
+
     {
       id: 2,
-      name: "Kota Ueshima",
-      position: "Software Engineer | Frontend Developer",
-      img: Kota,
+      name: "Atharva Babuna",
+      position: "Operations | Engineering",
+      img: Atharva,
     },
     {
       id: 3,
       name: "Kota Ueshima",
-      position: "Software Engineer | Frontend Developer",
+      position: "Frontend Developer",
       img: Kota,
     },
     {
       id: 4,
-      name: "Kota Ueshima",
-      position: "Software Engineer | Frontend Developer",
-      img: Kota,
+      name: "Shriya Padhi",
+      position: "Engineering",
+      img: Shriya,
+    },
+    {
+      id: 5,
+      name: "Sowmiya",
+      position: "Engineering",
+      img: Sowmiya,
     },
   ];
   return (
-    <div className="min-h-screen w-full mt-20 bg-droneBlue">
-      <div className="flex flex-col items-center text-white">
+    <div className="min-h-screen w-full mt-20 bg-gray-100">
+      <div className="flex flex-col items-center text-droneBlue">
         <h2 className="mt-20 text-4xl">
           Meet Our <span className="text-droneOrange">Team</span>
         </h2>
@@ -40,15 +51,18 @@ function TeamPage() {
           technology. We have experience at top technology and financial firms,
           and are growing rapidly.
         </p>
-        <div className="max-w-[75vw] mx-auto mt-20 flex flex-row space-x-20">
+        <div className="max-w-[75vw] mx-auto mt-20 flex flex-row justify-center space-x-16">
           {teamMembers.map(({ id, name, position, img }) => {
             return (
               <div key={id} className="text-center">
-                <Image
-                  src={img}
-                  alt={`Picture of ${name}`}
-                  className="rounded-md"
-                />
+                <div className="relative h-[50vh] w-[15vw]">
+                  <Image
+                    src={img}
+                    alt={`Picture of ${name}`}
+                    className="object-cover"
+                    fill={true}
+                  />
+                </div>
                 <h2 className="mt-4 text-lg">{name}</h2>
                 <p className="mt-2 text-sm">{position}</p>
               </div>
